09bde5bc064f3dfeb9b8a99c48633ac9ec77cc87
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-07-24 19:53:57 -0400

    trying to get weather info from ip

--- weather_app/views.py
+++ weather_app/views.py
@@ -4,14 +4,30 @@ from ipware import get_client_ip
 # Create your views here.
 
 def index(request):
+    #Get user ip address
     user_ip, is_routable = get_client_ip(request)
 
-    # context = {'city_weather':city_weather}
+    # Get user longitude and latitude
+    ip_response = requests.get(f'http://ip-api.com/json/{user_ip}').json()
+    print(ip_response)
+    user_lat = ip_response['lat']
+    user_lon = ip_response['lon']
+    city = ip_response['city']
 
-    print(user_ip, is_routable)
+    #OpenWeatherAPI - Weather info from lat and lon
+    open_weather_url= f'api.openweathermap.org/data/2.5/weather?lat={user_lat}&lon={user_lon}&appid=65d6a2854eae556a61d30eaa38bea1a6'
+    weather_response = requests.get(open_weather_url).json()
 
-    return render(request, 'weather_app/weather.html', {'user_ip':user_ip, 'is_routable':is_routable})
-    # remember to enter in context
+    city_weather = {
+        'user_ip':is_routable,
+        'city': city,
+        'temperature': weather_response['main']['temp'],
+        'description':weather_response[0]['description'],
+        'icon': weather_response['weather'][0]['description']
+    }
+        
+    return render(request, 'weather_app/weather.html', city_weather)
+
 
     if request.method=="POST":
         search_city = request.POST.get('search-city')
@@ -19,27 +35,10 @@ def index(request):
         return render(request, 'weather_app/weather.html',{})
 
     # else:
-    #     # Get user IP address
+
         
 
-    #     #user_ip = request.META.get("REMOTE_ADDR")
         
-    #     # Get user longitude and latitude
-    #     ip_response = requests.get(f'http://ip-api.com/json/{user_ip}').json()
-    #     print(ip_response)
-    #     user_lat = ip_response['lat']
-    #     user_lon = ip_response['lon']
-    #     city = ip_response['city']
-        
-    #     #OpenWeatherAPI
-    #     open_weather_url= f'api.openweathermap.org/data/2.5/weather?lat={user_lat}&lon={user_lon}&appid=65d6a2854eae556a61d30eaa38bea1a6'
-    #     weather_response = requests.get(open_weather_url).json()
 
-    #     city_weather = {
-    #         'user_ip':is_routable,
-    #         'city': city,
-    #         'temperature': weather_response['main']['temp'],
-    #         'description':weather_response[0]['description'],
-    #         'icon': weather_response['weather'][0]['description']
-    #     }
+
 
