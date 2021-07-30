d1a6686d63c6a6750f5658c5e929923192ff0acc
E-A-Gee <70771300+E-A-Gee@users.noreply.github.com>
2021-07-30 15:39:04 -0400

    add searchbar functionality

--- weather_app/views.py
+++ weather_app/views.py
@@ -4,19 +4,27 @@ from ipware import get_client_ip
 # Create your views here.
 
 def index(request):
-    #Get user ip address
-    user_ip, is_routable = get_client_ip(request)
+    #if using the searchbar
+    if request.method=="POST":
+        search_city = request.POST.get('search-city')
+        open_weather_url= f'http://api.openweathermap.org/data/2.5/weather?q={search_city}&units=imperial&appid=65d6a2854eae556a61d30eaa38bea1a6'
+        weather_response = requests.get(open_weather_url).json()
 
-    # Get user longitude and latitude
-    ip_response = requests.get(f'http://ip-api.com/json/{user_ip}').json()
-    
-    user_lat = ip_response['lat']
-    user_lon = ip_response['lon']
-    city = ip_response['city']
+    #if getting ip address
+    else:
+        #Get user ip address
+        user_ip, is_routable = get_client_ip(request)
 
-    #OpenWeatherAPI - Weather info from lat and lon
-    open_weather_url= f'http://api.openweathermap.org/data/2.5/weather?lat={user_lat}&lon={user_lon}&units=imperial&appid=65d6a2854eae556a61d30eaa38bea1a6'
-    weather_response = requests.get(open_weather_url).json()
+        # Get user longitude and latitude
+        ip_response = requests.get(f'http://ip-api.com/json/{user_ip}').json()
+        
+        user_lat = ip_response['lat']
+        user_lon = ip_response['lon']
+        city = ip_response['city']
+
+        #OpenWeatherAPI - Weather info from lat and lon
+        open_weather_url= f'http://api.openweathermap.org/data/2.5/weather?lat={user_lat}&lon={user_lon}&units=imperial&appid=65d6a2854eae556a61d30eaa38bea1a6'
+        weather_response = requests.get(open_weather_url).json()
 
     city_weather = {
         'city': city,
@@ -26,18 +34,3 @@ def index(request):
     }
         
     return render(request, 'weather_app/weather.html', city_weather)
-
-
-    if request.method=="POST":
-        search_city = request.POST.get('search-city')
-
-        return render(request, 'weather_app/weather.html',{})
-
-    # else:
-
-        
-
-        
-
-
-
