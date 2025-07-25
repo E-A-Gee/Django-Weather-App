31d8ec0c30c5e1fef4821da6cc12ee33fadb3d84
Elizabeth Gee <geelizikers@gmail.com>
2025-07-25 18:54:36 -0400

    Fix Debug Mode in Settings
    The environ variable would be the string 'True' or 'False',
    not a Python bool.

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('WEATHER_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('WEATHER_DEBUG')
+DEBUG = (os.environ.get('WEATHER_DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['geeliz-weather.up.railway.app']
 
